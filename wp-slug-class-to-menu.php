ce9d8348eb6ee83af3010c13363a43ccf5650557
Chetan Vaghela <51482283+thechetanvaghela@users.noreply.github.com>
2022-09-20 22:28:20 +0530

    Add slug as class to menu item

---
+++ wp-slug-class-to-menu.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Add the page or post slug as a class to a menu item.
+ * Add to functions.php
+ */
+function add_slug_class_to_menu_item($output){
+	$ps = get_option('permalink_structure');if(!empty($ps)){
+		$idstr = preg_match_all('/<li id="menu-item-(\d+)/', $output, $matches);
+		foreach($matches[1] as $mid){
+			$id = get_post_meta($mid, '_menu_item_object_id', true);
+			$slug = basename(get_permalink($id));
+			$output = preg_replace('/menu-item-'.$mid.'">/', 'menu-item-'.$mid.' menu-item-'.$slug.'">', $output, 1);
+		}
+	}
+	return $output;
+}
+add_filter('wp_nav_menu', 'add_slug_class_to_menu_item');
